cf27e9f1e13a3ac3bce76f1aa594650a1d6c7041
ToYama <toyama170402@svk.jp>
2022-03-23 11:06:02 +0900

    複数ファイル対応化

--- Program.cs
+++ Program.cs
@@ -10,27 +10,29 @@ namespace PDF_to_Jpeg
         static void Main(string[] args)
         {
             PdfDocument doc = new PdfDocument();
-
-            Console.WriteLine("ファイルパス:" + args[0]);
-            Console.WriteLine("ファイルを読み込み中");
-            //PDFファイルをロードします。
-            doc.LoadFromFile(args[0]);
-
-            //総ページ数をカウント
-            int allPageNumber = doc.Pages.Count;
-            Console.WriteLine("総ページ数" + allPageNumber + "ページ");
-
-            Console.WriteLine("Jpegに変換中");
-            //各ページを Jpegとして保存
-            for (int i = 0; i < allPageNumber; i++)
+            for (int i = 0; i < args.Length; i++)
             {
-                Image bmp = doc.SaveAsImage(i);
-                int pagenumber = i + 1;
-                string jpgpath = args[0] + pagenumber + ".jpeg";
-                string fileName = string.Format(jpgpath);
-                bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Jpeg);
-            }
+                Console.WriteLine("ファイルパス:" + args[i]);
 
+                //PDFファイルをロードします。
+                Console.WriteLine("ファイルを読み込み中");
+                doc.LoadFromFile(args[i]);
+
+                //総ページ数をカウント
+                int allPageNumber = doc.Pages.Count;
+                Console.WriteLine("総ページ数" + allPageNumber + "ページ");
+
+                //各ページを Jpegとして保存
+                Console.WriteLine("Jpegに変換中");
+                for (int j = 0; j < allPageNumber; j++)
+                {
+                    Image bmp = doc.SaveAsImage(j);
+                    int pagenumber = j + 1;
+                    string jpgpath = args[i] + pagenumber + ".jpeg";
+                    string fileName = string.Format(jpgpath);
+                    bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                }
+            }
         }
     }
 }
